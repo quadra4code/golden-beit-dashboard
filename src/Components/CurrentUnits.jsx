9928a638495ca50902d12b51c5b10ea04954dfd7
Mahmoud Elsonbaty <mahmoudelsonbaty777@gmail.com>
2025-06-19 19:58:11 +0300

    search V5

--- src/Components/CurrentUnits.jsx
+++ src/Components/CurrentUnits.jsx
@@ -126,7 +126,7 @@ const CurrentUnits = () => {
     axios.post('https://api.goldenbeit.com/dashboard/paginated-units',
       {
         status_id: statusFilter,
-        search_keyword: searchTerm ? `${searchTerm}${event.target.value}` : event.target.value
+        search_keyword: event.target.value
       },
       {
         headers: {
